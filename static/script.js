d1ecef3dcae3bf89ed1fc8343b8d7b1cd9e6c717
sabyacsgithub <137919277+sabyacsgithub@users.noreply.github.com>
2025-08-13 01:34:48 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function searchTable() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let rows = document.querySelectorAll("tbody tr");
+
+    rows.forEach(row => {
+        let text = row.innerText.toLowerCase();
+        row.style.display = text.includes(input) ? "" : "none";
+    });
+}
